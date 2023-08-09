021c1a3397b7c6658d7608899290f22f34b9f813
fredd <freddypcarrion@gmail.com>
2023-08-08 22:23:12 -0500

    requirements.txt

---
+++ test.py
@@ -0,0 +1,47 @@
+# Exercise 1
+Name = str(input("Enter a name"))
+print(f"Hello {Name}")
+
+# Exercise 2
+Name = str(input("Enter your name"))
+Surname = str(input("Enter your surname"))
+
+print(f"Hello {Name} {Surname}")
+
+# Exercise 4
+a = float(input("Enter a number"))
+b = float(input("Enter a number"))
+
+print(f"The total is {a + b}")
+
+# Exercise 5
+a = float(input("Enter a number"))
+b = float(input("Enter a number"))
+c = float(input("Enter a number"))
+
+print(f"The answer is {(a + b) / c}")
+
+# Exercise 7
+Name = str(input("Enter your name"))
+Age = int(input("Enter your age"))
+
+print(f"{Name} next birthday you will be {Age + 1}")
+
+# Exercise 8
+Price = float(input("what is the price?"))
+Dinners = int(input("How many dinners?"))
+
+print(f"Each person should pay USD {Price / Dinners}")
+
+# Exercise 9
+Days = int(input("Days"))
+Hours = Days * 24
+Minutes = Days * 1440
+Seconds = Days * 86400
+
+print(f"{Days} days contain {Hours} hours, {Minutes} minutes and {Seconds} seconds")
+
+# Exercise 10
+Weight = float(input("Enter your weight in kg"))
+
+print(f"Your weight in pounds is {Weight * 2.204} pounds")
